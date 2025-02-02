2d69f5ada584daac8be6d3c3e74b1856405dba79
Sargun Vohra <sargunv@users.noreply.github.com>
2025-02-02 01:09:01 -0800

    simplify times(Double) implementation

--- src/commonMain/kotlin/io/github/kevincianfarini/alchemist/internal/SaturatingLong.kt
+++ src/commonMain/kotlin/io/github/kevincianfarini/alchemist/internal/SaturatingLong.kt
@@ -87,20 +87,9 @@ internal value class SaturatingLong(val rawValue: Long) {
     }
 
     inline operator fun times(other: Double): SaturatingLong {
-        return when {
-            other.isNaN() -> throwIllegalArgumentException("Multiplying an value by NaN yields an undefined result.")
-            isInfinite() or other.isInfinite() -> when {
-                rawValue == 0L || other == 0.0 -> {
-                    throwIllegalArgumentException("Multiplying an infinite value by zero yields an undefined result.")
-                }
-                rawValue.sign == other.sign.roundToInt() -> POSITIVE_INFINITY
-                else -> NEGATIVE_INFINITY
-            }
-            else -> {
-                // TODO Handle overflow
-                SaturatingLong((rawValue * other).roundToLong())
-            }
-        }
+        val thisDouble = toDouble()
+        val result = thisDouble * other
+        return SaturatingLong(result.roundToLong())
     }
 
     operator fun div(other: SaturatingLong): SaturatingLong {
